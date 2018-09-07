c0e8850ec8542d612b64dfba34e00457209a1abc
Guillermo Gomez <guillermo.gomez.pernas@gmail.com>
2018-09-07 12:59:02 +0100

    Update UploadState.java
    added cancel event and added multiple uploads supports partially

--- src/android/UploadState.java
+++ src/android/UploadState.java
@@ -8,4 +8,5 @@ public class UploadState {
   public static final String UPLOADED = "UPLOADED";
   public static final String FAILED = "FAILED";
   public static final String STARTED = "STARTED";
+  public static final String CANCELLED = "CANCELLED";
 }
